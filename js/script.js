7ee37a94e6a4316e354b9ad333d5067a6592f19f
CasualFrost <simplyjasonfrost@gmail.com>
2021-11-30 15:07:47 -0800

    add readme, add condition for handling clicking outside image, clean up javascript file

--- js/script.js
+++ js/script.js
@@ -96,17 +96,16 @@ const masterBoard = [{
     },
 ]
 const grid = document.querySelector('.grid')
-// let lockBoard;
 let board, cardGuess, correctGuesses;
-
+document.querySelector('button').addEventListener('click', init);
+document.querySelector('.grid').addEventListener('click', cardClicked);
+init();
 function init() {
     board = shuffleBoard();
     cardGuess = [];
     correctGuesses = [];
-    // lockBoard = false;
     createBoard();
 }
-
 function createBoard() {
     grid.innerHTML = "";
     for (let i = 0; i < board.length; i++) {
@@ -116,84 +115,43 @@ function createBoard() {
         grid.appendChild(cards)
     }
 }
-document.querySelector('button').addEventListener('click', init);
-document.querySelector('.grid').addEventListener('click', cardClicked);
-//  function cardClicked(evt) {
-//     let imageElements = document.querySelectorAll('img')
-//     let cardIdx = parseInt(evt.target.dataset.id)
-//     imageElements[cardIdx].src = board[cardIdx].img
-// }
-// function cardClicked(evt) {
-//     let imageEls = document.querySelectorAll('img');
-//     let cardIdx = parseInt(evt.target.dataset.id);
-//     if (cardGuess.length < 2) {
-//         cardGuess.push(board[cardIdx].name);
-//     }
-//     if (cardGuess.length === 2) checkMatch();
-//     imageEls[cardIdx].src = board[cardIdx].img;
-// }
 function cardClicked(evt) {
-    // find index from evt.target
     let cardIdx = parseInt(evt.target.dataset.id);
-
-    // if length of cardGuess array is less than 2, add the index to cardGuess
-    // then render that card over on the DOM with a renderCard function that accepts an index number as an argument and flips the correct card
+    if (cardGuess.includes(cardIdx)) return;
+    if (evt.target.tagName !== 'IMG') return;
+    if (cardGuess.length === 2) return;
     if (cardGuess.length < 2) {
         cardGuess.push(cardIdx);
         renderCard(cardIdx);
     }
-
-    // if user has made two guesses (cardGuess.length === 2), check for match in separate function
     if (cardGuess.length === 2) checkMatch();
-
 }
-
-// accepts cardIdx as argument
 function renderCard(cardIdx) {
-    // select all img elements from DOM with querySelectorAll
     let imageEls = document.querySelectorAll("img");
 
-    // use cardIdx to set source of img to url from board array at that index
     imageEls[cardIdx].src = board[cardIdx].img;
 }
-
-// using the indexes in cardGuess, look at that index in the board array
-// and figure out if there is a match
 function checkMatch() {
-    // if there is a match
     if (board[cardGuess[0]].name === board[cardGuess[1]].name) {
-    // add current guesses to correct guesses array
         correctGuesses.push(cardGuess[0]);
         correctGuesses.push(cardGuess[1]);   
-    // reset cardGuess back to empty array
        cardGuess = [];
-       // if no match, just reset cardGuess back to empty array
     } else {
        cardGuess = [];
     }
-    // re-render the whole board to show correct guesses after 1.5 seconds
-    // use setTimeout to call a separate renderBoard function
     setTimeout(renderBoard, 1500);
-    // renderBoard();
 }
-// should render the whole board to show what cards have been correctly guessed
 function renderBoard() {
-    // select all img elements from DOM with querySelectorAll
     let imageEls = document.querySelectorAll('img');
-    // using forEach, iterate over all image elements
     imageEls.forEach(function(img, idx) {
-        // if the index of the image element is in the correctGuesses array
         if (correctGuesses.includes(idx)) {
-            // show the snowflake like we did in renderCard
             imageEls[idx].src = board[idx].img;
-            // add border (or something) to show it has been correctly guessed
             img.style.border = '5px solid lime';
-            // if idx is not in correctGuesses, show the front of the card
+            img.style.pointerEvents = 'none';
         } else {
             imageEls[idx].setAttribute('src', 'https://i.imgur.com/RibM4vq.png')
         }
     })
-
 }
 function shuffleBoard() {
     let boardCopy = [...masterBoard];
@@ -204,4 +162,3 @@ function shuffleBoard() {
     }
     return newBoard;
 }
-init();
---
+++ readme.md
@@ -0,0 +1,63 @@
+# Frosty Memory Game
+
+Thanks for visiting my [Frosty Memory Game](https://casualfrost.github.io/Frosty-Memory-Game/)! Created by CasualFrost.
+
+## Description
+
+Concentration, also known as Matching Pairs, Match Match, Match Up, Memory, Pleonasm, Shank, Pexeso or simply Pairs, is a card game in which all of the cards are laid face down on a surface and two cards are flipped face up over each turn. The object of the game is to turn over pairs of matching cards.
+
+![Game](https://i.imgur.com/TIMqELh.png)
+
+### Technologies Used
+
+1. HTML
+2. CSS
+3. JavaScript
+
+## Getting Started
+
+In order to start playing the browser game, click on this [game link](https://casualfrost.github.io/Frosty-Memory-Game/).
+
+There are 12 snowflakes to match for a total of 24 images that need to be flipped over. 
+
+In order to get started simply click on your first guess and then proceed to click on a second guess. 
+
+If both are matched then it will lock in place with a green border.
+
+If both aren't a match then they will flip back over in the same spot. 
+
+This game targets your memory skills as some of the snowflakes are similar to each other leading to a challenging experience. 
+
+Once you match all 12 unique snowflakes then you win! Good Luck. 
+
+Instructions are also in game.
+
+### Next Steps
+
+1. Animate the images swapping between each other.
+
+2. Double click restriction so no accidental double clicks will lock an image in place.
+
+3. Add lives to the game, each failed match will lower the life of the player. If player fails enough times then player loses and game resets.
+
+4. Add more difficulties/levels. Ex: Easy = 8 matches, Normal = 12 matches, Hard = 20 matches, Impossible = 40 matches.
+
+5. More animations and background changed, possibly make it so how many lives left affect color
+
+## Random Facts
+
+- When laying out the cards of a Concentration game ready for play, they are often arranged in a neat square pattern or other format to make memorising their location somewhat easier.
+
+- A good strategy in Concentration is to first flip over the card that you are unsure of, before flipping the card that you are certain of, in case you remember incorrectly.
+
+- There are numerous forms of the game of Concentration, and the cards are often square in shape, although some simply using traditional playing cards, while others use unique printed designs that often contain pictures, however, each set will have the same colour and design on the back of each of the cards.
+
+- Children generally prefer to play the game of Concentration, and usually do well at the game, although adults do play to test their memorisation skills.
+
+- On the assumption that each already revealed card is memorised in a game of Concentration, the probability of flipping over a matching card can generally be defined through the formula 1/(t-1-n), where ‘t’ is the remaining cards in play and ‘n’ is the already revealed cards that are still in play.
+
+- The person with the most pairs at the end of a game of Concentration is deemed the winner, and while the game is typically played competitively with two or more players, it can be enjoyed by a single player aiming to flip the least amount of cards but revealing the most pairs, or to simply test their memory.
+
+- A Japanese version of the game of Concentration, known as ‘Kai-awase’ and made from painted clam shells, is said to have been played by the wealthy, as early as the 9th century.
+
+[Random Facts Source](https://www.tenrandomfacts.com/concentration/)
